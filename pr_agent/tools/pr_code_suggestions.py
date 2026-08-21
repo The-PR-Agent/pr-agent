a7b6824f2c9da020b3cf808ae3840081247d2174
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2026-08-21 18:05:26 +0330

    fix(pr_agent/tools/pr_code_suggestions.py): requiring code_suggestions to be a list

--- pr_agent/tools/pr_code_suggestions.py
+++ pr_agent/tools/pr_code_suggestions.py
@@ -591,10 +591,10 @@ class PRCodeSuggestions:
                          first_key="code_suggestions", last_key="label")
         if isinstance(data, list):
             data = {'code_suggestions': data}
-        if not isinstance(data, dict) or 'code_suggestions' not in data:
+        if not isinstance(data, dict) or not isinstance(data.get("code_suggestions"), list):
             get_logger().error("Failed to parse code suggestions from the AI prediction",
-                               artifact={'predictions': predictions})
-            return {'code_suggestions': []}
+                               artifact={"predictions": predictions})
+            return {"code_suggestions": []}
 
         # remove or edit invalid suggestions
         suggestion_list = []
