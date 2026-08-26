1fff27e1ddf8c41b830aad8e2f2ca55de4dd3d2a
even-even <>
2026-08-26 23:10:32 +0300

    fix after uncommiting isort

--- tests/e2e_tests/test_bitbucket_app.py
+++ tests/e2e_tests/test_bitbucket_app.py
@@ -1,10 +1,8 @@
-import hashlib
 import os
-import re
+import os
 import time
 from datetime import datetime
 
-import jwt
 import requests
 from atlassian.bitbucket import Cloud
 from requests.auth import HTTPBasicAuth
@@ -12,9 +10,7 @@ from requests.auth import HTTPBasicAuth
 from pr_agent.config_loader import get_settings
 from pr_agent.log import get_logger, setup_logger
 from tests.e2e_tests.e2e_utils import (FILE_PATH,
-                                       IMPROVE_START_WITH_REGEX_PATTERN,
-                                       NEW_FILE_CONTENT, NUM_MINUTES,
-                                       PR_HEADER_START_WITH, REVIEW_START_WITH)
+                                       NEW_FILE_CONTENT, NUM_MINUTES)
 
 log_level = os.environ.get("LOG_LEVEL", "INFO")
 setup_logger(log_level)
